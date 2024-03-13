e9236b21a6f1a91ca4923f20541537cc80d63d62
Captain Jay <captainstorage183@gmail.com>
2024-03-13 20:46:01 +0530

    fixed special character

--- app/components/Wcu.tsx
+++ app/components/Wcu.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 export default function Wcu() {
   return (
     <>
-    <div className='rounded-3xl w-full'>
-    <div className="text-6xl text-white inline justify-self-center">Why choose us?</div>
+      <div className='rounded-3xl w-full'>
+        <div className="text-6xl text-white inline justify-self-center">Why choose us?</div>
 
-    <div className="text-lg text-white mt-8">
-    Discover unparalleled care at our physiotherapy clinic, where your well-being is our top priority. Our experienced and compassionate team utilizes state-of-the-art facilities to tailor comprehensive treatment plans, ensuring personalized and effective rehabilitation. With a patient-centered approach, we prioritize your comfort and progress, fostering open communication. Located conveniently with flexible scheduling, we've earned a 5-star rating on Google Maps, a testament to the positive experiences and trust our patients place in our care. Choose us for a journey to recovery that begins with excellence
-    </div>
-    </div>
+        <div className="text-lg text-white mt-8">
+          Discover unparalleled care at our physiotherapy clinic, where your well-being is our top priority. Our experienced and compassionate team utilizes state-of-the-art facilities to tailor comprehensive treatment plans, ensuring personalized and effective rehabilitation. With a patient-centered approach, we prioritize your comfort and progress, fostering open communication. Located conveniently with flexible scheduling, we&apos;ve earned a 5-star rating on Google Maps, a testament to the positive experiences and trust our patients place in our care. Choose us for a journey to recovery that begins with excellence
+        </div>
+      </div>
     </>
-  )
+  );
 }
